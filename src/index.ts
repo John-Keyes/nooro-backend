c7be7656b67114a9a320d6d7233f3e4ea9924bd5
John <johnschool432@gmail.com>
2025-01-06 14:34:10 -0500

    cleaning comments

--- src/index.ts
+++ src/index.ts
@@ -16,15 +16,10 @@ const main = async () => {
   app.use(express.json());
   app.use(express.urlencoded({extended: true}));
 
-  //app.use(cors());
   const corsOptions = {
     origin: fullClientUrl
   };
   app.use(cors(corsOptions));
-
-  //curl -H "Origin: http://localhost:4040" --head http://localhost:3040/tasks
-
-  // Register API routes
   app.use("/", (req: Request, res: Response, next) => {
     res.set({
       "Access-Control-Allow-Origin": fullClientUrl,
@@ -32,11 +27,6 @@ const main = async () => {
     });
     next();
   }, routes);
-  /*app.use("/", (req: Request, res: Response, next) => {
-    res.header("Access-Control-Allow-Origin", fullClientUrl);
-    res.header("Content-Type", "Application/JSON");
-    next();
-  })*/
 
   app.listen(port);
 }
